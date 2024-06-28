6aff4531d647700c0ddb033c5d28dcdb8b140598
Fabiá Lovato <fawlosc@gmail.com>
2024-06-28 13:03:03 -0500

    líneas

--- Logic/LineasAereas.cs
+++ Logic/LineasAereas.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using SGF.DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,11 @@ namespace SGF.BussinessLogic
 {
     public partial class Logic
     {
+        [OperationContract]
+        public SGF_LineaAerea LineaAerea_ObtenerPorID(Guid id)
+        {
+            DataModel model = new DataModel();
+            return model.SGF_LineaAerea.First(x => x.LineaAereaID == id);
+        }
     }
 }
